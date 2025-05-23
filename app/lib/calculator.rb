639182d3943d6566c657e533d9823316bc7908d1
Siddhesh Jadhav <siddhesh.jadhav@gocomet.com>
2025-05-23 17:48:11 +0530

    test: return single number if only one number is given

--- app/lib/calculator.rb
+++ app/lib/calculator.rb
@@ -1,5 +1,6 @@
 class Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
+    return numbers.to_i if numbers.match?(/^\d+$/)
   end
 end
--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Calculator do
     it 'returns 0 for an empty string' do
       expect(Calculator.add("")).to eq(0)
     end
+
+    it 'returns the number itself if there is only one number' do
+      expect(Calculator.add("1")).to eq(1)
+    end
   end
 end
